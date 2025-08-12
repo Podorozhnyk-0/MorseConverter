9c9c8a74f7d5204529077604f8f6159ceef17c8f
Podorozhnyk <mrpodoroznyk@gmail.com>
2025-08-13 00:18:19 +0300

    Finished UI

---
+++ src/main/java/ru/podorozhnyk/application/AuthorInfo.java
@@ -0,0 +1,9 @@
+package ru.podorozhnyk.application;
+
+public final class AuthorInfo {
+    private AuthorInfo() {
+    }
+
+    public static final String AUTHOR_NAME = "Podorozhnyk-0";
+    public static final String GITHUB_LINK = "https://github.com/Podorozhnyk-0";
+}
